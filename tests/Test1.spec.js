af9c4658e9b345abf562e6e083edc358b235325d
Hars <harsimaran.singh25@outlook.com>
2025-10-04 18:36:08 +1000

    addedtest

---
+++ tests/Test1.spec.js
@@ -0,0 +1,3 @@
+// lets jsut write a simple practice program for typescript
+var message = 'This is a string message';
+console.log(message);
---
+++ tests/Test1.spec.ts
@@ -0,0 +1,4 @@
+// lets jsut write a simple practice program for typescript
+
+let message:string ='This is a string message';
+console.log(message);
