37328bfa480c7f9d114d724c749af05ccb6158a7
Hars <harsimaran.singh25@outlook.com>
2025-10-05 12:14:59 +1100

    added

--- tests/demo.js
+++ tests/demo.js
@@ -1,2 +1,12 @@
 var message = 'This is a string message';
 console.log(message);
+// how to write function in ts
+function add(a, b) {
+    return a + b;
+}
+add(3, 4);
+//objects in ts
+var person = { name: 'bob', age: 34 };
+console.log('non interface', person.name);
+var person1 = { name: 'bob', age: 34 };
+console.log(person1.name);
